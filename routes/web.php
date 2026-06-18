a1a37b23193056d98277b51d2f7051b9d8b01e40
Luiz Felipe <lepinholima@gmail.com>
2026-06-18 10:46:22 -0300

    refactor: remove useless files

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\LocaleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
 
 Route::get('/locale/{locale}', LocaleController::class)->name('set-locale');
